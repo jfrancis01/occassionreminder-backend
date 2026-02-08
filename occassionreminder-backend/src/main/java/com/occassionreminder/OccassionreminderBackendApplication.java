bfe0ecc6f66d861544145cb8788df4d2edb91be5
jfrancis01 <joelfrancis@rogers.com>
2026-02-08 00:27:44 -0500

    made some pomfile changes

--- occassionreminder-backend/src/main/java/com/occassionreminder/OccassionreminderBackendApplication.java
+++ occassionreminder-backend/src/main/java/com/occassionreminder/OccassionreminderBackendApplication.java
@@ -2,14 +2,21 @@ package com.occassionreminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableWebSecurity
-public class OccassionreminderBackendApplication {
+public class OccassionreminderBackendApplication extends SpringBootServletInitializer {	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OccassionreminderBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OccassionreminderBackendApplication.class, args);
